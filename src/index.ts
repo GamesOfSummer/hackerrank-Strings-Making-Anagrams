76a0079930586f1b1b509555c807d4872283b01d
Summer T <summer@summertime-development.com>
2023-04-11 11:06:19 -0500

    progress

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,48 @@ function makeAnagram(a: string, b: string): number {
         aSet.add(aArray[i]);
     }
 
-    const holder = aSet;
+    //***** */
 
+    let bArray = b.split('');
+    let bSet = new Set();
+    for (let i = 0; i < bArray.length; i++) {
+        bSet.add(bArray[i]);
+    }
+
+    //***** */
+
+    const holder = aSet;
+    const holder2 = bSet;
     //********************
-    return 4;
+
+    let duplicateSet = new Set();
+
+    let counter = 0;
+    for (let i = 0; i < aSet.size; i++) {
+        if (!bSet.has(aSet[i])) {
+            counter++;
+            duplicateSet.add(aSet[i]);
+        }
+    }
+
+    const holder3 = duplicateSet;
+
+    for (let i = 0; i < bSet.size; i++) {
+        if (!aSet.has(bSet[i]) && !duplicateSet.has(bSet[i])) {
+            counter++;
+            duplicateSet.add(bSet[i]);
+        }
+    }
+
+    return counter;
 }
 
 consoleStart();
+
+validateFxn(
+    makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'),
+    30
+);
 
 validateFxn(makeAnagram('cde', 'abc'), 4);
 validateFxn(makeAnagram('cdeee', 'abc'), 4);
