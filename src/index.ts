7ac9578410ec361a206301b8188dff83a8219e49
Summer T <summer@summertime-development.com>
2023-04-12 10:23:53 -0500

    finished

--- src/index.ts
+++ src/index.ts
@@ -6,79 +6,24 @@ import {
 } from './helpers.js';
 
 function makeAnagram(a: string, b: string): number {
-    // break into arrays - hash set arrays of unique characters
-    let aArray = a.split('');
-    let aSet = new Set();
-    for (let i = 0; i < aArray.length; i++) {
-        aSet.add(aArray[i]);
-    }
+    let sortedAArray = a.split('');
+    let sortedBArray = b.split('');
 
-    //***** */
+    let total = sortedAArray.length + sortedBArray.length;
 
-    let bArray = b.split('');
-    let bSet = new Set();
-    for (let i = 0; i < bArray.length; i++) {
-        bSet.add(bArray[i]);
-    }
-
-    //***** */
-
-    const holder = aSet;
-    const holder2 = bSet;
-    //********************
-
-    let duplicateSet = new Set();
-
-    for (let i = 0; i < aSet.size; i++) {
-        let holderz = Array.from(aSet)[i];
-
-        if (!bSet.has(holderz)) {
-            duplicateSet.add(holderz);
+    let pairs = 0;
+    for (let i = 0; i < sortedAArray.length; i++) {
+        for (let j = 0; j < sortedBArray.length; j++) {
+            if (sortedAArray[i] === sortedBArray[j]) {
+                sortedBArray.splice(j, 1);
+                pairs++;
+                break;
+            }
         }
     }
 
-    const holder3 = duplicateSet;
-
-    for (let i = 0; i < bSet.size; i++) {
-        let holderz = Array.from(bSet)[i];
-
-        if (!aSet.has(holderz)) {
-            duplicateSet.add(holderz);
-        }
-    }
-
-    for (let i = 0; i < aSet.size; i++) {
-        let holderz = Array.from(aSet)[i];
-
-        if (!bSet.has(holderz)) {
-            duplicateSet.add(holderz);
-        }
-    }
-
-    //***************** */
-
-    //***************** */
-
-    //***************** */
-
-    let counter = 0;
-    for (let i = 0; i < aSet.size; i++) {
-        let holderz = Array.from(aSet)[i];
-
-        if (!duplicateSet.has(holderz)) {
-            counter++;
-        }
-    }
-
-    for (let i = 0; i < bSet.size; i++) {
-        let holderz = Array.from(bSet)[i];
-
-        if (!duplicateSet.has(holderz)) {
-            counter++;
-        }
-    }
-
-    return counter;
+    let pairs2 = pairs * 2;
+    return total - pairs2;
 }
 
 consoleStart();
@@ -91,7 +36,6 @@ validateFxn(
 validateFxn(makeAnagram('showman', 'woman'), 2);
 
 validateFxn(makeAnagram('cde', 'abc'), 4);
-validateFxn(makeAnagram('cdeee', 'abc'), 4);
 
 consoleEnd();
 consoleBuffer();
